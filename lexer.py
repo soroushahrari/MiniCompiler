640993ad4c4cb7e8a4c292ebe5f64e211aa38536
Soroush Ahrari <ahrari.soroush76@gmail.com>
2020-05-22 15:57:34 +0430

    nesting level added

--- lexer.py
+++ lexer.py
@@ -1,6 +1,9 @@
 from sly import Lexer
 
 class MyLexer(Lexer):
+    def __init__(self):
+        self.nesting_level = 0
+
     tokens = {
         INTEGER, FLOAT, STRING, BOOLEAN,
         IF, ELSE, FOR, WHILE, SWITCH, CASE,
@@ -46,6 +49,18 @@ class MyLexer(Lexer):
         t.value = int(t.value)
         return t
 
+    @_(r'\{')
+    def lbrace(self, t):
+        t.value = '{'
+        self.nesting_level +=1
+        return t
+    
+    @_(r'\}')
+    def rbrace(self, t):
+        t.value = '}'
+        self.nesting_level -= 1
+        return t
+
     IDENTIFIER = r'[a-zA-Z_]+[a-zA-Z0-9_]*'
     IDENTIFIER['if'] = IF
     IDENTIFIER['else'] = ELSE
@@ -64,14 +79,29 @@ class MyLexer(Lexer):
     def ignore_newline(self, t):
         self.lineno += t.value.count('\n')
 
+    def find_column(text, token):
+        last_cr = text.rfind('\n', 0, token.index)
+        if last_cr < 0:
+            last_cr = 0
+        column = (token.index - last_cr) + 1
+        return column
+
     def error(self, t):
         print(f'ERROR! Line {self.lineno}: Bad Character {t.value[0]}')
         self.index += 1
     
 
-if __name__ == "__main__":
-    data = 'xif if else != >>= = 4 + 5.4 bool true falsef //hello \n yes : hello switch case""' 
-    lexer = MyLexer()
+# if __name__ == "__main__":
+#     data = '''
+#         int x = 0;
+#         while (x < 10) {
+#             x = x + 2
+#         }
+#     ''' 
+#     lexer = MyLexer()
 
-    for token in lexer.tokenize(data):
-        print(token)
+#     for token in lexer.tokenize(data):
+#         print(MyLexer.find_column(data, token), token.type, token.value, f'nesting level: {lexer.nesting_level}')
+#         # print(token)
+
+    
