be746cda5f620c15685a18c72c704bd94482a9cb
Soroush Ahrari <ahrari.soroush76@gmail.com>
2020-08-11 23:04:22 +0430

    read file from argv and parse the data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
 from lexer import MyLexer
+from parser import MyParser
 from symbolTable import SymTable
-
 
 # Read source code from file
 def filereader(filename):
@@ -9,21 +10,50 @@ def filereader(filename):
     return data
 
 if __name__ == "__main__":
-    fileName = input('Enter your file Address: ex: test/test.txt\n')
-    data = filereader(fileName)
-    lexer = MyLexer()
+    # fileName = input('Enter your file Address: ex: test/test.txt\n')
+    # data = filereader(fileName)
     mySymbolTable = SymTable()
-    tokens = lexer.tokenize(data)
+    lexer = MyLexer()
+    parser = MyParser()
+    # tokens = lexer.tokenize(data)
     list_of_tokens = []
 
-    # Insert identifiers into symbol table
-    for token in tokens:
-        list_of_tokens.append(token)
-        if token.type == 'IDENTIFIER':    
-            mySymbolTable.insert(token)
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: python3 -m main.py filename \n')
+        raise SystemExit(1)
 
-    print('Symbol Table =>', mySymbolTable.symbols)
-    print('-----------------------------')
-    print('List of All tokens\n')
-    for token in list_of_tokens:
-        print(token)
+    text = filereader(sys.argv[1])
+    result = parser.parse(lexer.tokenize(text))
+
+    for item in result:
+        print(item)
+
+    # while True:
+    #     try:
+    #         text = input('test > ')
+    #     except EOFError:
+    #         break
+
+    #     if text:
+    #         tokens = lexer.tokenize(text)
+    #         res = parser.parse(tokens)
+    #         print(res)
+    #         parser.log
+    #         print(parser.symstack)
+
+    # print(parser.symstack)
+        # text = input('comp > ')
+    
+    # Insert identifiers into symbol table
+    # for token in tokens:
+    #     list_of_tokens.append(token)
+    #     if token.type == 'IDENTIFIER':    
+    #         mySymbolTable.insert(token)
+
+    # print('Symbol Table =>', mySymbolTable.symbols)
+    # print('-----------------------------')
+    # print('List of All tokens\n')
+    # for token in list_of_tokens:
+    #     print(token)
+
+    
